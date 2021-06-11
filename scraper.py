ef7d4ef575c6296146f3fa4d357b92262e7cca17
Patrick Yevych <patrick@DESKTOP-44MIL9D.localdomain>
2021-06-11 00:19:18 -0400

    Add url scraping

--- scraper.py
+++ scraper.py
@@ -2,17 +2,21 @@ from typing import List
 from bs4 import BeautifulSoup
 import requests, re
 
-HEADERS = {'User-Agent': 'Mozilla/5.0'}
-STRS_BLACKLIST = ['×', 'Search tools', 'Recent', 'Sorted by relevance', 'SW_C_X', 'Sign in']
-# Google supports 10 results per page for up to 1000 total results
 PER_PAGE = 10 # default: 10
-MAX_RES = 100 # default: 1000
-GOOGLE_DATE_PATTERN = "^\d+ (seconds?|minutes?|hours?|days?|months?|years?) ago$"
+MAX_RES = 20 # default: 1000
 
+HEADERS = {'User-Agent': 'Mozilla/5.0'}
+
+STRS_BLACKLIST = ['×', 'Search tools', 'Recent', 'Sorted by relevance', 'SW_C_X', 'Sign in']
+GOOGLE_LINK_PATTERN = r"^https?:\/\/.*\.google\..*$"
+
+LINK_HEAD_PATTERN = r"(\/url\?q=)(https?:\/\/.*)"
+GOOGLE_DATE_PATTERN = r"^\d+ (seconds?|minutes?|hours?|days?|months?|years?) ago$"
 
 def get_news_objs(query: str, page: int = 0) -> BeautifulSoup:
     req = requests.get('https://www.google.com/search?q='+query+'&source=lnms&tbm=nws&start='+str(page), allow_redirects=False, headers=HEADERS)
     return BeautifulSoup(req.text, 'html.parser')
+
 
 def get_strs(news_objs: BeautifulSoup) -> List[str]:
     res = []
@@ -21,6 +25,7 @@ def get_strs(news_objs: BeautifulSoup) -> List[str]:
             res.append(div.string)
     return res
 
+
 def get_spans(news_objs: BeautifulSoup) -> List[str]:
     res = []
     for span in news_objs.find_all('span'):
@@ -28,14 +33,34 @@ def get_spans(news_objs: BeautifulSoup) -> List[str]:
             res.append(span.string)
     return res
 
+
+def get_href(news_objs: BeautifulSoup) -> List[str]:
+    res = []
+    for href in news_objs.find_all("a",href=re.compile(LINK_HEAD_PATTERN)):
+        link = re.split(LINK_HEAD_PATTERN, href['href'])[2]
+        if re.match(GOOGLE_LINK_PATTERN, link) == None:
+            res.append(link)
+    return res
+
+
+def get_urls(query: str) -> List[str]:
+    res = []
+    for i in range(0, MAX_RES, PER_PAGE):
+        links = get_href(get_news_objs(query, i))
+        for link in links:
+                res.append(link)
+    return res
+
+
 def get_dates(query: str) -> List[str]:
     res = []
     for i in range(0, MAX_RES, PER_PAGE):
         spans = get_spans(get_news_objs(query, i))
         for span in spans:
-            if re.match(GOOGLE_DATE_PATTERN, span) != None:
+            if re.match(GOOGLE_DATE_PATTERN, span) != None and span:
                 res.append(span)
     return res
+
 
 def get_headlines(query: str) -> List[str]: 
     res = []
@@ -46,19 +71,19 @@ def get_headlines(query: str) -> List[str]:
                 res.append(t)
     return res
 
+
 def build_table(query: str) -> List[str]:
     res = []
     
     hdlnl = get_headlines(query)
     dates = get_dates(query)
+    urls = get_urls(query)
     
     for i in range(0, min(len(hdlnl)-1, len(dates))):
-        res.append([hdlnl[2*i+1], hdlnl[2*i], dates[i]])
+        res.append([hdlnl[2*i+1], hdlnl[2*i], dates[i], urls[2*i]])
     return res
+
 
 print(build_table('bitcoin'))
 
-
-
-# need to have date and url as well; url primary key
 # scrap multiple search engines: bing, duckduck go.
