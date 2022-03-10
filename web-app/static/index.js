adb7a2fe6f08681d073b722878ecc2b721afa758
patrick <patrick.yevych39@gmail.com>
2022-03-10 01:35:49 +0000

    Add table row onclick handler

--- web-app/static/index.js
+++ web-app/static/index.js
@@ -12,10 +12,26 @@ closeScraper.onclick = function() {
     scraperForm.style.display = "none";
 }
 
-// set pause button visibility
+
 for (i = 0; i < scraperTable.rows.length; i++) {
+    // set pause button visibility
     var row = scraperTable.rows[i];
     if (row.cells[6].innerHTML == "manual") {
         row.cells[8].getElementsByClassName("toggle-scraper-form")[0].getElementsByClassName("link-button").toggle_btn.style.display = "none";
+    }
+
+    // set onclick handler
+    var cellClickHandler = function(row) {
+        return function() {
+            var query = row.getElementsByTagName("td")[0].innerHTML;
+            var engine = row.getElementsByTagName("td")[1].innerHTML;
+            window.location.href = "./results/"+engine+"/"+query;
+        }
+    }
+
+    for (j = 0; j < row.cells.length; j++) {
+        if (j != 8) {
+            row.cells[j].onclick = cellClickHandler(row);
+        }
     }
 }
